165a9488133435c2ba05587e74884a3a86247b77
Abhishek10075 <aknishad6677@gmail.com>
2026-07-17 18:17:48 +0530

    updated code

--- 6_Array/14_two_sum_problem.py
+++ 6_Array/14_two_sum_problem.py
@@ -11,7 +11,45 @@ class Solution:
                     return True   # or [i, j]
 
         return False
+'''
+TC=o(N*N)
+SC=o(N)
+'''
 
 #Solution 2->Better
-
 		  
+class Solution(object):
+    def twoSum(self, nums, target):
+        n=len(nums)-1
+        hashmap={}
+        for i in range(0,n+1):
+            reamaining=target-nums[i]
+            if  reamaining in hashmap:
+                return [hashmap[ reamaining],i]
+            hashmap[nums[i]]=i
+'''
+TC=o(n)
+SC=o(n)
+'''
+
+#Solution 3->optimal
+def twoSum(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+        current_sum = nums[left] + nums[right]
+
+        if current_sum == target:
+            return [left, right]
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+
+    return []
+
+'''
+TC=o(n)
+SC=o(1)
+'''
