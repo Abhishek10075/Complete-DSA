e0b9a9002db1806dddea7d871a931ade02068952
Abhishek10075 <aknishad6677@gmail.com>
2026-07-20 16:57:51 +0530

    update dsa code

--- 6_Array/17_maximum_subarray.py
+++ 6_Array/17_maximum_subarray.py
@@ -10,3 +10,27 @@ class Solution:
                 sum+=arr[j]
                 maxi=max(maxi,sum)
         return maxi
+'''
+TC=o(n*n)
+SC=o(1)
+'''
+
+#Solution 2->optimal 
+class Solution(object):
+    def maxSubArray(self, nums):
+        n=len(nums)-1
+       
+        max_sum=float('-inf')
+        cur_sum=0
+        for i in range(0,n+1):
+            cur_sum=cur_sum+nums[i]
+            max_sum=max(cur_sum,max_sum)
+            if cur_sum<0:
+                cur_sum=0
+    
+        return max_sum
+        
+'''
+TC=o(n)
+SC=o(1)
+'''
