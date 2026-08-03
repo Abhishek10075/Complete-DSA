34c5adbdf5f1a3ff7eb9127fc6a78b0965f9817f
Abhishek Nishad <aknishad6677@gmail.com>
2026-08-03 15:43:48 +0530

    updated code

--- 3_Recursion/4_str_palindrome.py
+++ 3_Recursion/4_str_palindrome.py
@@ -33,7 +33,48 @@ class Solution:
             
         
 
-         
+#check sentence is palindrome or not ->brute solution
+class Solution(object):
+    def isPalindrome(self, s):
+        valid_string=''
+        s=s.split()
+        s=''.join(s)
+        for i in s:
+            if i.isalpha() or i.isdigit():
+                valid_string+=i
+        valid_string=valid_string.lower()
+        n=len(valid_string)
+        left=0
+        right=n-1
+        pali=True
+        while left<=right:
+            if valid_string[left]!=valid_string[right]:
+                pali=False
+            left+=1
+            right-=1
+        if pali:
+            return True
+        else:
+            return False
+#Palindrome Check (Alphanumeric)
+class Solution(object):
+    def isPalindrome(self, s):
+        valid=[]
+        for ch in s:
+            if ch.isalnum():
+                valid.append(ch.lower())
+        n=len(valid)
+        left=0
+        right=n-1
+        while left<right:
+            if valid[left]!=valid[right]:
+                return False
+            left+=1
+            right-=1
+        return True
+
+
+
             
         
 
--- 6_Array/1_Easy/9_Merge_2_sorted_array.py
+++ 6_Array/1_Easy/9_Merge_2_sorted_array.py
@@ -31,3 +31,28 @@ class Solution:
 TC=TC(n+m)
 SC=SC(n+m)
 '''
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        i, j = 0, 0
+        result = []
+
+        while i < m and j < n:
+            if nums1[i] <= nums2[j]:
+                result.append(nums1[i])
+                i += 1
+            else:
+                result.append(nums2[j])
+                j += 1
+
+        while i < m:
+            result.append(nums1[i])
+            i += 1
+
+        while j < n:
+            result.append(nums2[j])
+            j += 1
+
+        # Copy result back into nums1
+        for k in range(m + n):
+            nums1[k] = result[k]
