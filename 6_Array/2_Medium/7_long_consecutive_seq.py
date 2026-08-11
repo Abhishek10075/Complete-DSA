f2bc3c9b85608e4a65e6b5bad726ab1ad6d6e214
Abhishek Nishad <aknishad6677@gmail.com>
2026-08-11 19:36:31 +0530

    updated code

--- 6_Array/2_Medium/7_long_consecutive_seq.py
+++ 6_Array/2_Medium/7_long_consecutive_seq.py
@@ -19,3 +19,35 @@ class Solution(object):
 TC=o(n*n)
 sc=o(1)
 '''
+
+#Solution 2->Better
+class Solution(object):
+    def longestConsecutive(self, nums):
+        nums.sort()
+        n=len(nums)-1
+        count=0
+        last_smaller=float('-inf')
+        longest=0
+        for i in range(0,n+1):
+
+            #nums =[0,1,1,2]
+            if i==0:
+                count=1
+                last_smaller=nums[0]
+            if nums[i] == last_smaller:
+                continue
+            elif nums[i] - 1 == last_smaller:
+                count += 1
+                last_smaller = nums[i]
+            else:
+                longest=max(longest,count)
+                count = 1
+                last_smaller = nums[i]
+        longest=max(longest,count)
+        return longest
+
+
+'''
+TC=n log(n) + o(n)
+SC=o(1)
+'''
