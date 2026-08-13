5ebddbd0f74b9dcdf9d7db4996f417f680b9210b
Abhishek Nishad <aknishad6677@gmail.com>
2026-08-13 19:15:19 +0530

    set zero matrix

--- 6_Array/2_Medium/8_set_matrix_zero.py
+++ 6_Array/2_Medium/8_set_matrix_zero.py
@@ -32,3 +32,33 @@ class Solution(object):
 TC=o((n+m)*(n*m)+(n*m))
 SC=o(1)
 '''
+
+#optimal solution 
+class Solution(object):
+
+    def setZeroes(self, matrix):
+        r = len(matrix)
+        c = len(matrix[0])
+
+        row_track = [0] * r
+        col_track = [0] * c
+
+        # Track rows and columns containing 0
+        for i in range(r):
+            for j in range(c):
+                if matrix[i][j] == 0:
+                    row_track[i] = -1
+                    col_track[j] = -1
+
+        # Set corresponding rows and columns to 0
+        for i in range(r):
+            for j in range(c):
+                if row_track[i] == -1 or col_track[j] == -1:
+                    matrix[i][j] = 0
+
+        return matrix
+
+'''
+TC=o(2(m*n))~ o(m*n)
+SC=o(m+n)
+'''
