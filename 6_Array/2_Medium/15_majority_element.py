0d4504b0309b432960e3e9d14ce9121089973d5e
Abhishek Nishad <aknishad6677@gmail.com>
2026-08-15 20:31:18 +0530

    updated code

--- 6_Array/2_Medium/15_majority_element.py
+++ 6_Array/2_Medium/15_majority_element.py
@@ -18,3 +18,23 @@ TC=o(n*n)+o(n log n)
 SC=o(1)
 '''
 
+#Better using dictionary
+class Solution(object):
+    def majorityElement(self, nums):
+        n=len(nums)
+        frequence={}
+        major_ele=[]
+        for i in range(0,n):
+            if nums[i] not in frequence:
+                frequence[nums[i]]=1
+            else:
+                 frequence[nums[i]]+=1
+        for k,v in frequence.items():
+            if v>n//3:
+                major_ele.append(k)
+        return major_ele
+                
+'''
+TC=o(n)+o(n)~o(n)
+SC=O(n)
+'''
