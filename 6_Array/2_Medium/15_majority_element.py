1ea9f1a8517bce972ecd1be8cc49b46b83d8585d
Abhishek Nishad <aknishad6677@gmail.com>
2026-08-15 21:09:15 +0530

    updated code

--- 6_Array/2_Medium/15_majority_element.py
+++ 6_Array/2_Medium/15_majority_element.py
@@ -38,3 +38,56 @@ class Solution(object):
 TC=o(n)+o(n)~o(n)
 SC=O(n)
 '''
+
+#Optimal solution
+class Solution(object):
+    def majorityElement(self, nums):
+        n = len(nums)
+
+        count1, count2 = 0, 0
+        ele1, ele2 = 0, 0
+
+        # Find candidates
+        for i in range(n):
+            if count1 == 0 and nums[i] != ele2:
+                ele1 = nums[i]
+                count1 = 1
+
+            elif count2 == 0 and nums[i] != ele1:
+                ele2 = nums[i]
+                count2 = 1
+
+            elif nums[i] == ele1:
+                count1 += 1
+
+            elif nums[i] == ele2:
+                count2 += 1
+
+            else:
+                count1 -= 1
+                count2 -= 1
+
+        # Verify candidates
+        c1, c2 = 0, 0
+
+        for i in range(n):
+            if nums[i] == ele1:
+                c1 += 1
+
+            elif nums[i] == ele2:
+                c2 += 1
+
+        # Return valid majority elements
+        result = []
+
+        if c1 > n // 3:
+            result.append(ele1)
+
+        if c2 > n // 3:
+            result.append(ele2)
+
+        return result
+    '''
+    TC=o(n)
+    SC=o(1)
+    '''
