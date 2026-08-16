820c452294c90d9ff07d3e3175d48c011daae920
Abhishek Nishad <aknishad6677@gmail.com>
2026-08-16 08:20:57 +0530

    Binary Search

---
+++ 7_Binary Search/1_Binary_Search.py
@@ -0,0 +1,21 @@
+
+class Solution(object):
+    def search(self, nums, target):
+        n=len(nums)-1
+        low=0
+        high=n
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return mid
+            if target>nums[mid]:
+                low=mid+1
+            else:
+                high=mid-1
+        return -1
+'''
+TC=o(n/2)
+SC=o(1)
+'''
+
+
