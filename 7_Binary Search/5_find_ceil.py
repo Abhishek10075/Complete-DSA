0e7c93e8c0a323b1315b7cb2ed754a6d63fa3ad8
Abhishek Nishad <aknishad6677@gmail.com>
2026-08-18 12:35:50 +0530

    find ceil

--- 7_Binary Search/5_find_ceil.py
+++ 7_Binary Search/5_find_ceil.py
@@ -1,3 +1,11 @@
+#ceil means greater than or equal to the given number
+'''
+example: arr=[1,2,8,10,10,12,19], x=5
+output: 2 (index of 8)
+because 8 is the smallest number which is greater than or equal to 5
+TC=o(log n)
+SC=o(1)
+'''
 class Solution:
     def findCeil(self, arr, x):
         # code here
