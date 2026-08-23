e353747fcc6cb28b231c2835c9a49d396db80514
Abhishek Nishad <aknishad6677@gmail.com>
2026-08-23 13:33:25 +0530

    find occurrence of number

--- 7_Binary Search/7_count_occurrences_num.py
+++ 7_Binary Search/7_count_occurrences_num.py
@@ -15,3 +15,46 @@ class Solution:
             
         else:
             return last-first+1
+
+
+#Optimal Solution
+class Solution:
+    def lowerbound(self,arr,target):
+        n=len(arr)
+        l=0
+        r=n-1
+        lb=-1
+        while l<=r:
+            mid=(l+r)//2
+            if arr[mid]>=target:
+                lb=mid
+                r=mid-1
+            else:
+                l=mid+1
+        return lb
+        
+    def upperbound(self,arr,target):
+        n=len(arr)
+        l=0
+        r=n-1
+        ub=n #when target is greater than all elements in the array
+        while l<=r:
+            mid=(l+r)//2
+            if arr[mid]>target:
+                ub=mid
+                r=mid-1
+            else:
+                l=mid+1
+        return ub
+        
+    def countFreq(self, arr, target):
+        # code here
+        n=len(arr)-1
+        lower=self.lowerbound(arr,target)
+        if lower==-1:
+            return 0
+        upper=self.upperbound(arr,target)
+        
+        return upper-lower
+
+    
