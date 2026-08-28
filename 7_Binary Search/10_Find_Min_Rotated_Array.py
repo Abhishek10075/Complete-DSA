8d8fc566a9c34d77dc8d9a8bf0f82e0273123bfe
Abhishek Nishad <aknishad6677@gmail.com>
2026-08-28 18:44:08 +0530

    find elements in a rotated sorted array

--- 7_Binary Search/10_Find_Min_Rotated_Array.py
+++ 7_Binary Search/10_Find_Min_Rotated_Array.py
@@ -1,16 +1,33 @@
-#Find Minimum in Rotated Sorted Array
+#Find Minimum in Rotated Sorted Array->Brute Force
+class Solution(object):
+    def findMin(self, nums):
+        n=len(nums)-1
+        mini=float('inf')
+        for i in range(0,n+1):
+            if nums[i]<mini:
+                mini=nums[i]
+        return mini
+
+#Optimal solution->Binary Search
 class Solution(object):
     def findMin(self, nums):
         n = len(nums) - 1
+        mini = float('inf')
         l = 0
         r = n
 
-        while l < r:
+        while l <= r:
             mid = (l + r) // 2
 
-            if nums[mid] > nums[r]:
+            # Left part is sorted
+            if nums[l] <= nums[mid]:
+                mini = min(mini, nums[l])
                 l = mid + 1
-            else:
-                r = mid
 
-        return nums[l]
+            # Right part is sorted
+            else:
+                mini = min(mini, nums[mid])
+                r = mid - 1
+
+        return mini
+    
