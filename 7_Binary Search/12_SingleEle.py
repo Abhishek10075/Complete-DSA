6cb530ce039b3cfd87cd713667e3b35cf57d289a
Abhishek Nishad <aknishad6677@gmail.com>
2026-09-04 12:36:50 +0530

    single element in sorted array

---
+++ 7_Binary Search/12_SingleEle.py
@@ -0,0 +1,8 @@
+#Find the single element in sorted array.
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        for i in range(0, len(nums) - 1, 2):
+            if nums[i] != nums[i + 1]:
+                return nums[i]
+        
+        return nums[-1]
