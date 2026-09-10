b67dd2f57e59567ca336187861fb015363314ed8
Abhishek Nishad <aknishad6677@gmail.com>
2026-09-10 08:16:30 +0530

    updated code

--- 6_Array/1_Easy/9_Merge_2_sorted_array.py
+++ 6_Array/1_Easy/9_Merge_2_sorted_array.py
@@ -56,3 +56,27 @@ class Solution(object):
         # Copy result back into nums1
         for k in range(m + n):
             nums1[k] = result[k]
+
+
+
+    #optimal solution
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        i=m-1
+        j=n-1
+        k=m+n-1
+
+        while i>=0 and j>=0:
+            if nums1[i]>nums2[j]:
+                nums1[k]=nums1[i]
+                i-=1
+            else:
+                nums1[k]=nums2[j]
+                j-=1
+            k-=1
+        while j>=0: #may be nums2 has some elements left ,nums1 has all elements in place
+            nums1[k]=nums2[j]
+            j-=1
+            k-=1
+
+        return nums1
