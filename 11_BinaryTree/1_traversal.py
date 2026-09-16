beec6858e11bce416a8b11757ec5f157e82dddba
Abhishek Nishad <aknishad6677@gmail.com>
2026-09-16 19:52:55 +0530

    Preorder Traversal of Binary Tree

--- 11_BinaryTree/1_traversal.py
+++ 11_BinaryTree/1_traversal.py
@@ -1,15 +1,21 @@
+#Pre Order Traversal
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution(object):
-    def inorderTraversal(self, root):
-        result = []
-
-        def inorder(node):
+    def preorderTraversal(self, root):
+        result=[]
+        def preorder(node):
             if node is None:
-                return
+                return 
+            result.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
 
-            inorder(node.left)       # Left
-            result.append(node.val)  # Root
-            inorder(node.right)      # Right
-
-        inorder(root)
+        preorder(root)
 
         return result
