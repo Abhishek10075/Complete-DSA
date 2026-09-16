6a3e6a4b152f6c2c86c28e1cb7f8254158f4a4c7
Abhishek Nishad <aknishad6677@gmail.com>
2026-09-16 20:11:33 +0530

    inorder traversal

--- 11_BinaryTree/1_traversal.py
+++ 11_BinaryTree/1_traversal.py
@@ -19,3 +19,29 @@ class Solution(object):
         preorder(root)
 
         return result
+
+'''
+TC=o(n) where n is number of nodes in tree
+SC=o(h) where h is height of tree
+'''
+
+#In order Traversal
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        result=[]
+        def inorder(node):
+            if node is None:
+                return
+            inorder(node.left)
+            result.append(node.val)
+            inorder(node.right)
+        inorder(root)
+
+        return result
+        
